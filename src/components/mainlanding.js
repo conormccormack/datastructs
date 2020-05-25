953662221b58a5380284dd4c7ab8e406da1d3ef4
Conor McCormack <conormcc@usc.edu>
2020-05-25 03:41:31 -0400

    datastructs

--- src/components/mainlanding.js
+++ src/components/mainlanding.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components'
 
 const MainPanel = styled.div`
-    display: grid;
     grid-template-columns: 1fr 1fr;
     padding-top: 20px;
 `
@@ -15,37 +14,47 @@ const WelcomeHeadline = styled.div`
 const WelcomeBody = styled.div`
     font-family: Raleway;
     font-size: 16px;
-    width: 600px
+`
+
+const MainWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
 `
 
 const WelcomeMessage =styled.div`
-    padding-right: 5vw;
+    grid-column: 2 / 3;
+    padding-right: 100px;
 `
 
 const MainGraphic = styled.div`
-    max-width: 600px;
-    padding-left: 3vw;
+    width: 600px;
+    grid-column: 1 / 2;
+    padding-right: 30px;
+    padding-left: 120px;
 `
 
 function Mainlanding(){
     return (
         <MainPanel>
-            <MainGraphic>
-                <img src={require("../resources/images/logo_waves.svg")} alt="logo waves"></img>
-            </MainGraphic>
-            <WelcomeMessage>
-                <WelcomeHeadline>Welcome to DataStructs</WelcomeHeadline>
+            <MainWrapper>
+                <MainGraphic>
+                    <img src={require("../resources/images/logo_waves.svg")} alt="logo waves"></img>
+                </MainGraphic>
+                <WelcomeMessage>
+                    <WelcomeHeadline>Deadly Waves</WelcomeHeadline>
                     <WelcomeBody>
-                DataStructs is a beautiful, intuitive platform for vizualizing essential data structures and algorithms.
-                    <br/><br/>
-                Each page includes buttery-smooth animations of common data structures that you can build and use <strong>without
+                        DataStructs is a beautiful, intuitive platform for vizualizing essential data structures and algorithms.
+                        <br/><br/>
+                        Each page includes buttery-smooth animations of common data structures that you can build and use <strong>without
                         writing a single line of code.</strong><br/><br/>
-                On top of that, DataStructs provides explanations and automated notes for every animation, so anyone can
-                    build an intuition each approach’s motivating ideas and asymptotic behavior. <br/><br/>
-                Open the explore menu to browse the collection categorically, click everything to see 💯, or search for a
-                    specific data structure or concept in the search bar.
+                        On top of that, DataStructs provides explanations and automated notes for every animation, so anyone can
+                        build an intuition each approach’s motivating ideas and asymptotic behavior. <br/><br/>
+                        Open the explore menu to browse the collection categorically, click everything to see 💯, or search for a
+                        specific data structure or concept in the search bar.
                     </WelcomeBody>
-            </WelcomeMessage>
+                </WelcomeMessage>
+
+            </MainWrapper>
 
         </MainPanel>
     )
