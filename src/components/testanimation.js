d78e327d81a57497987c963a642660a3240f8b74
Conor McCormack <conormcc@usc.edu>
2020-05-26 01:44:15 -0400

    Added insert functionality to test animation * Added input box allowing users to input values for animated nodes * When user hits enter key, event handler generates new node * Framer Motion animates transition in and scaling onHover

--- src/components/testanimation.js
+++ src/components/testanimation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import Navbar from "./navbar";
@@ -7,35 +7,74 @@ import Navbar from "./navbar";
 const TestSandbox = styled.div`
     height: 100vh;
     background-color: #EFE7E2;
+    grid-template-columns: 1fr 1fr;
     display: grid;
 `
 
-const Centered = styled.div`
+const ListWrapper = styled.div`
     height: 200px; 
     width: 100px;
-    margin: auto;
-    text-align: center;
+    margin-left: 100px;
+    margin-right: 100px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
 `
 
-const Box = styled.div`
+const Node = styled.div`
     background-color: #D8BBFF; 
     border-radius: 50%;
-    margin-top: 10px;
+    margin: 20px;
     height: 100px; 
     width: 100px;
     margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const NodeContent = styled.div`
+    text-align: center;
+    vertical-align: middle;
+    font-size: 30px;
+    font-family: open-sans;
+    
+`
+
+const Margin20 = styled.div`
+    margin: 20px;
 `
 
 function TestAnimation(){
+    const [nodeList, setNodeList] = useState([]);
+    const [inputValue, setInputValue ] = useState([]);
+
+    const onInsertDown = event => {
+        event.preventDefault();
+        setNodeList(nodeList.concat(<Margin20>
+            <motion.div initial={{ scale: 0 }} animate={{ scale : 1}} whileHover={{ scale: 1.5 }} >
+                <Node>
+                    <NodeContent>{inputValue}</NodeContent>
+                </Node>
+            </motion.div>
+        </Margin20>));
+    };
+
+    const handleChange = event => setInputValue(event.target.value);
+
     return (
-        <TestSandbox>
+        <div>
             <Navbar/>
-            <Centered>
-                <motion.div whileHover={{ scale: 3}} >
-                       <Box/>
-                </motion.div>
-            </Centered>
-        </TestSandbox>
+            <TestSandbox>
+                <form style={{paddingLeft:'100px'}} onSubmit={onInsertDown}>
+                    <input type='text' value={inputValue} onChange={handleChange}></input>
+                    Insert
+                </form>
+
+                <ListWrapper>
+                    {nodeList}
+                </ListWrapper>
+            </TestSandbox>
+        </div>
     )
 
 }
